2aa05ccd086bd97654e9daea0d967c81d17e46a5
ZahraSH67 <zahra.shahabi.dk@gmail.com>
2025-11-10 14:54:57 +0100

    added Formal fullname

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function getFullName(firstName, surname){
-    return firstName + " " + surname
+function getFullName(firstName = "John", surname = "Smith", useFormalName = false, gender = "") {
+ 
+    if (useFormalName === true) {
+        if (gender === "Male") {
+            return "Lord " + firstName + " " + surname;
+        } else if (gender === "Female") {
+            return "Lady " + firstName + " " + surname;
+        } else {
+            return "Lord " + firstName + " " + surname; // Default 
+        }
+    }
+    return firstName + " " + surname;
 }
-const fullNam1 = getFullName("Zahra", "Shahabi");
-console.log(fullNam1);
-const fullName2 = getFullName("Emma", "Watson")
-console.log(fullName2)
+
+const fullName1 = getFullName("Zahra", "Shahabi", true, "Female");
+console.log(fullName1);
+
+const fullName2 = getFullName("Emma", "Watson");
+console.log(fullName2);
+
+const fullName3 = getFullName("Benjamin", "Hughes", true, "Male");
+console.log(fullName3);
+
+const fullName4 = getFullName("Benjamin", "Hughes", false);
+console.log(fullName4);
+
+const fullName5 = getFullName("", "");
+console.log(fullName5); // Should show error message
+const fullName6 = getFullName();
+console.log(fullName6); 
