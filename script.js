bf43690b34f583205b7fc0f56545c1d583ae0ebc
ZahraSH67 <zahra.shahabi.dk@gmail.com>
2025-11-10 14:30:01 +0100

    Added Flight booking fullname function

---
+++ script.js
@@ -0,0 +1,7 @@
+function getFullName(firstName, surname){
+    return firstName + " " + surname
+}
+const fullNam1 = getFullName("Zahra", "Shahabi");
+console.log(fullNam1);
+const fullName2 = getFullName("Emma", "Watson")
+console.log(fullName2)
